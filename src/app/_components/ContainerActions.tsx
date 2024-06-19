6930c4765983d1797ba56f5e790745aa6cb467c0
Daniel Haas <daniel.haas@wareload.net>
2024-06-19 04:58:41 +0200

    refactor

--- src/app/_components/ContainerActions.tsx
+++ src/app/_components/ContainerActions.tsx
@@ -3,26 +3,41 @@ import {api} from "wl/trpc/react";
 import {useRouter} from "next/navigation";
 import {CssSmallButton} from "wl/app/_utils/Css";
 import {type ContainerInfo} from "dockerode";
+import {type UseMutationOptions, type UseMutationResult} from "@tanstack/react-query";
 
 export default function ContainerActions(input: { item: ContainerInfo }) {
     const router = useRouter();
-    const useMutationOf = (mutationFn: { useMutation: any}) => mutationFn.useMutation({
-        onSuccess: () => router.refresh(),
-        onError: (e: Error) => console.error(e),
+    const useMutationOf = (mutationFn: {
+        useMutation: (opts?: UseMutationOptions<void, object, {
+            id: string
+        }>) => UseMutationResult<void, object, {
+            id: string
+        }>;
+    }) => mutationFn.useMutation({
+        onSuccess: () => {
+            router.refresh()
+        },
+        onError: (e: object) => {
+            console.error(e)
+        },
     })
-    const actionConfigs = [
-        {action: 'start', mutation: useMutationOf(api.docker.startContainer), allowedVisibilityState: 'exited'},
-        {action: 'restart', mutation: useMutationOf(api.docker.restartContainer), allowedVisibilityState: undefined},
-        {action: 'pause', mutation: useMutationOf(api.docker.pauseContainer), allowedVisibilityState: 'running'},
-        {action: 'unpause', mutation: useMutationOf(api.docker.unpauseContainer), allowedVisibilityState: 'paused'},
-        {action: 'stop', mutation: useMutationOf(api.docker.stopContainer), allowedVisibilityState: 'running'},
-        {action: 'remove', mutation: useMutationOf(api.docker.removeContainer), allowedVisibilityState: 'exited'},
+    const actionConfigs: {
+        action: string,
+        mutation: UseMutationResult<void, object, { id: string }>,
+        requiredVisibilityState: string | undefined
+    } [] = [
+        {action: 'start', mutation: useMutationOf(api.docker.startContainer), requiredVisibilityState: 'exited'},
+        {action: 'restart', mutation: useMutationOf(api.docker.restartContainer), requiredVisibilityState: undefined},
+        {action: 'pause', mutation: useMutationOf(api.docker.pauseContainer), requiredVisibilityState: 'running'},
+        {action: 'unpause', mutation: useMutationOf(api.docker.unpauseContainer), requiredVisibilityState: 'paused'},
+        {action: 'stop', mutation: useMutationOf(api.docker.stopContainer), requiredVisibilityState: 'running'},
+        {action: 'remove', mutation: useMutationOf(api.docker.removeContainer), requiredVisibilityState: 'exited'},
     ];
     return <div className="flex flex-row gap-2 my-1">
         {actionConfigs.map((item) => <button key={"Button: " + item.action + input.item.Id} type="button"
                                              onClick={() => {
                                                  item.mutation.mutate({id: input.item.Id})
                                              }}
-                                             className={CssSmallButton + (item.allowedVisibilityState && (input.item.State !== item.allowedVisibilityState ? " hidden" : ""))}>{item.action}</button>)}
+                                             className={CssSmallButton + (item.requiredVisibilityState && (input.item.State !== item.requiredVisibilityState ? " hidden" : ""))}>{item.action}</button>)}
     </div>
 }
